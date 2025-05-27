89f5e98779bf8171fd4f08b5bd2bf3e3a0f6bef0
Alexander Ursu <alexanderu@crytek.de>
2025-05-27 09:19:01 +0200

    Reverted flag require_existing back

--- plugins/filter/validate_partitions.py
+++ plugins/filter/validate_partitions.py
@@ -1,13 +1,13 @@
 from ansible_collections.aursu.lvm_setup.plugins.plugin_utils.filter_helpers import Disk
 
-def validate_partitions(parted_info, parts, default_label="gpt"):
+def validate_partitions(parted_info, parts, default_label="gpt", require_existing=False):
     disk = Disk.from_parted(parted_info)
 
     req = Disk(disk.disk, parts)
     req.set_state(disk)
     req.set_table(default_label)
     
-    return req.plan(required=False)
+    return req.plan(required=require_existing)
 
 class FilterModule(object):
     def filters(self):
