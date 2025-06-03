91ba3a5066db13fecd4d204cf0bdbc367d72773a
Alexander Ursu <alexanderu@crytek.de>
2025-06-03 16:36:33 +0200

    Eliminated use of test_lv_base as it caused last 3 tests' failures

--- tests/unit/plugins/filter/test_validate_munt.py
+++ tests/unit/plugins/filter/test_validate_munt.py
@@ -2,53 +2,46 @@ import pytest
 from ansible.errors import AnsibleFilterError
 from ansible_collections.aursu.lvm_setup.plugins.filter.validate_mount import validate_mount
 
-# Unit tests
-test_lv_base = {
-    "name": "data1",
-    "vg": "data",
-    "size": "100g"
-}
-
-@pytest.mark.parametrize("lv_mod, dev_info, expected", [
+@pytest.mark.parametrize("lv, dev_info, expected", [
     # 1. Properly mounted volume
     (
-        {"mountpoint": "/mnt/data1", "filesystem": "xfs"},
+        {"name": "data1", "vg": "data", "size": "100g", "mountpoint": "/mnt/data1", "filesystem": "xfs"},
         {"is_exists": True, "mount": [{"target": "/mnt/data1"}]},
         True
     ),
     # 2. Exists but not mounted
     (
-        {"mountpoint": "/mnt/data1", "filesystem": "xfs"},
+        {"name": "data1", "vg": "data", "size": "100g", "mountpoint": "/mnt/data1", "filesystem": "xfs"},
         {"is_exists": True, "mount": [{"target": "/wrong"}]},
         False
     ),
     # 3. Exists, no mount section
     (
-        {"mountpoint": "/mnt/data1", "filesystem": "xfs"},
+        {"name": "data1", "vg": "data", "size": "100g", "mountpoint": "/mnt/data1", "filesystem": "xfs"},
         {"is_exists": True},
         False
     ),
     # 4. Not exists
     (
-        {"mountpoint": "/mnt/data1", "filesystem": "xfs"},
+        {"name": "data1", "vg": "data", "size": "100g", "mountpoint": "/mnt/data1", "filesystem": "xfs"},
         {"is_exists": False},
         False
     ),
     # 5. Invalid mountpoint (not absolute path)
     (
-        {"mountpoint": "mnt/data1", "filesystem": "xfs"},
+        {"name": "data1", "vg": "data", "size": "100g", "mountpoint": "mnt/data1", "filesystem": "xfs"},
         {"is_exists": True, "mount": [{"target": "mnt/data1"}]},
         AnsibleFilterError
     ),
     # 6. Invalid filesystem type
     (
-        {"mountpoint": "/mnt/data1", "filesystem": "ntfs"},
+        {"name": "data1", "vg": "data", "size": "100g", "mountpoint": "/mnt/data1", "filesystem": "ntfs"},
         {"is_exists": True, "mount": [{"target": "/mnt/data1"}]},
         AnsibleFilterError
     ),
     # 7. dev_info is not a dict
     (
-        {"mountpoint": "/mnt/data1", "filesystem": "xfs"},
+        {"name": "data1", "vg": "data", "size": "100g", "mountpoint": "/mnt/data1", "filesystem": "xfs"},
         "not-a-dict",
         AnsibleFilterError
     ),
@@ -60,32 +53,25 @@ test_lv_base = {
     ),
     # 9. missing 'name'
     (
-        {"vg": "data", "size": "100g"},
+        {"vg": "data", "size": "100g", "mountpoint": "/mnt/data1", "filesystem": "xfs"},
         {"is_exists": True},
         AnsibleFilterError
     ),
     # 10. missing 'vg'
     (
-        {"name": "data1", "size": "100g"},
+        {"name": "data1", "size": "100g", "mountpoint": "/mnt/data1", "filesystem": "xfs"},
         {"is_exists": True},
         AnsibleFilterError
     ),
     # 11. missing 'size'
     (
-        {"name": "data1", "vg": "data"},
+        {"name": "data1", "vg": "data", "mountpoint": "/mnt/data1", "filesystem": "xfs"},
         {"is_exists": True},
         AnsibleFilterError
     ),
 ])
 
-def test_validate_mount(lv_mod, dev_info, expected):
-    lv = dict(test_lv_base)
-
-    if isinstance(lv_mod, dict):
-        lv.update(lv_mod)
-    else:
-        lv = lv_mod
-
+def test_validate_mount(lv, dev_info, expected):
     if isinstance(expected, type) and issubclass(expected, Exception):
         with pytest.raises(expected):
             validate_mount(lv, dev_info)
