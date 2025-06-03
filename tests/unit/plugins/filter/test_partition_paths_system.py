5b22a9038675f4f686bb83c64814f5b3ada5d335
Alexander Ursu <alexanderu@crytek.de>
2025-06-03 21:22:48 +0200

    Fixed typo in unit test

--- tests/unit/plugins/filter/test_partition_paths_system.py
+++ tests/unit/plugins/filter/test_partition_paths_system.py
@@ -2,7 +2,7 @@ import pytest
 from ansible.errors import AnsibleFilterError
 from ansible_collections.aursu.lvm_setup.plugins.filter.partition_paths_system import partition_paths_system
 
-def partition_paths_system():
+def test_partition_paths_system_valid():
     partitions = {
         "/dev/sda": [{"num": 1, "size": "100g"}, {"num": 2}],
         "/dev/nvme0n1": [{"num": 1}]
@@ -10,6 +10,6 @@ def partition_paths_system():
     result = partition_paths_system(partitions)
     assert result == "/dev/sda1,/dev/sda2,/dev/nvme0n1p1"
 
-def test_partition_paths_all_invalid_input():
+def test_partition_paths_system_invalid_input():
     with pytest.raises(AnsibleFilterError, match="Expected 'partitions' to be a dictionary."):
         partition_paths_system("not a dictionary")
