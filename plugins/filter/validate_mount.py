b30dbf233480735cd28b2bc65c49617b5a9eb084
Alexander Ursu <alexanderu@crytek.de>
2025-06-03 15:50:20 +0200

    Corrected validate_mount filter

--- plugins/filter/validate_mount.py
+++ plugins/filter/validate_mount.py
@@ -1,10 +1,10 @@
 from ansible_collections.aursu.lvm_setup.plugins.plugin_utils.lvm_helpers import LogicalVolume, Device
 
 def validate_mount(lv, dev_info):
-    dev = Device.from_dev_info(dev_info)
     volume = LogicalVolume(lv)
+    dev = Device.from_dev_info(volume.path, dev_info)
 
-    if dev.is_exists() and volume.validate():
+    if dev.is_exists and volume.validate():
         return dev.validate_mount(volume.mount)
 
     return False
